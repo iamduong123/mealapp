d34d17f8a47e826fd4ea6b59e06e4a4564ca0542
iamduong123 <htduong.22003@gmail.com>
2024-03-23 14:08:17 +0700

    update 2.1

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,151 +1,71 @@
-import {
-  View,
-  Text,
-  ScrollView,
-  SafeAreaView,
-  Image,
-  TextInput,
-} from "react-native";
-import React, { useEffect, useState } from "react";
-import {
-  MagnifyingGlassIcon,
-  AdjustmentsHorizontalIcon,
-} from "react-native-heroicons/outline";
-import { StatusBar } from "expo-status-bar";
-import { heightPercentageToDP as hp } from "react-native-responsive-screen";
-import Categories from "../components/Categories";
-import axios from "axios";
-import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
-import Recipes from "../components/Recipes";
+import React from "react";
+import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import Button from "../components/Button";
 
-export default function HomeScreen() {
-  const [activeCategory, setActiveCategory] = useState("Beef");
-  const [categories, setCategories] = useState([]);
-  const [meals, setMeals] = useState([]);
-
-  useEffect(() => {
-    getCategories();
-    getRecipes();
-  }, []);
-
-  const handleChangeCategory = (category) => {
-    getRecipes(category);
-    setActiveCategory(category);
-    setMeals([]);
-  };
-
-  const getCategories = async () => {
-    try {
-      const response = await axios.get(
-        "https://www.themealdb.com/api/json/v1/1/categories.php"
-      );
-      if (response && response.data) {
-        setCategories(response.data.categories);
-        console.log(response.data.categories);
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const getRecipes = async (category = "Beef") => {
-    try {
-      const response = await axios.get(
-        `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
-      );
-      if (response && response.data) {
-        setMeals(response.data.meals);
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
+export default function Home({ navigation, routes }) {
   return (
-    <View className="flex-1 bg-white">
-      <StatusBar style="dark" />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>HOME</Text>
+      </View>
+      <View style={styles.content}>
+        <ScrollView contentContainerStyle={styles.scrollViewContent}>
+          <View style={styles.categoryContainer}>
 
-      <SafeAreaView>
-        <ScrollView
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{
-            paddingBottom: 50,
-          }}
-          className="space-y-6 pt-14"
-        >
-          {/* Avatar and Bell Icon */}
-          <View className="mx-4 flex-row justify-between items-center">
-            <AdjustmentsHorizontalIcon size={hp(4)} color={"gray"} />
-            <Image
-              source={require("../assets/images/avatar.png")}
-              style={{
-                width: hp(5),
-                height: hp(5),
-                resizeMode: "cover",
-              }}
-              className="rounded-full"
-            />
+            <Button style={styles.category} title={"Favorites"} onPress={() => navigation.navigate('Favorites')}>
+            </Button>
+            {CATEGORIES.map((category) => (
+              <Button key={category.id} style={[styles.category, { backgroundColor: category.color }]} title={category.title} onPress={() => navigation.navigate('Meal List', { data: category.id })}>
+              </Button>
+            ))}
           </View>
 
-          {/* Headlines */}
-          <View className="mx-4 space-y-1 mb-2">
-            <View>
-              <Text
-                style={{
-                  fontSize: hp(3.5),
-                }}
-                className="font-bold text-neutral-800"
-              >
-                Fast & Delicious
-              </Text>
-            </View>
-
-            <Text
-              style={{
-                fontSize: hp(3.5),
-              }}
-              className="font-extrabold text-neutral-800"
-            >
-              Food You <Text className="text-[#f64e32]">Love</Text>
-            </Text>
-          </View>
-
-          {/* Search Bar */}
-          <View className="mx-4 flex-row items-center border rounded-xl border-black p-[6px]">
-            <View className="bg-white rounded-full p-2">
-              <MagnifyingGlassIcon
-                size={hp(2.5)}
-                color={"gray"}
-                strokeWidth={3}
-              />
-            </View>
-            <TextInput
-              placeholder="Search Your Favorite Food"
-              placeholderTextColor={"gray"}
-              style={{
-                fontSize: hp(1.7),
-              }}
-              className="flex-1 text-base mb-1 pl-1 tracking-widest"
-            />
-          </View>
-
-          {/* Categories */}
-          <View>
-            {categories.length > 0 && (
-              <Categories
-                categories={categories}
-                activeCategory={activeCategory}
-                handleChangeCategory={handleChangeCategory}
-              />
-            )}
-          </View>
-
-          {/* Recipes Meal */}
-          <View>
-            <Recipes meals={meals} categories={categories} />
-          </View>
         </ScrollView>
-      </SafeAreaView>
+      </View>
+
     </View>
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 30
+  },
+  content: {
+    flex: 5,
+  },
+  scrollViewContent: {
+    justifyContent: "space-between",
+    alignItems: 'center',
+    padding: 10,
+  },
+
+  categoryContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    padding: 10,
+  },
+  category: {
+    backgroundColor: "#ccc",
+    borderRadius: 5,
+    // paddingHorizontal: '25%',
+    width: '100%',
+    paddingVertical: '25%',
+    marginRight: 5,
+    marginBottom: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
--- src/screens/RecipeListScreen.js
+++ src/screens/RecipeListScreen.js
@@ -1,77 +1,151 @@
-import React from "react";
-import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
-import { CATEGORIES, MEALS } from "../data/dummy-data";
-import Button from '../components/Button'
+import {
+  View,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  Image,
+  TextInput,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  MagnifyingGlassIcon,
+  AdjustmentsHorizontalIcon,
+} from "react-native-heroicons/outline";
+import { StatusBar } from "expo-status-bar";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import Categories from "../components/Categories";
+import axios from "axios";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
+import Recipes from "../components/Recipes";
 
-export default function RecipeListScreen({ navigation, route }) {
-  const data = route.params.data;
-  const category=CATEGORIES.find((cat) => cat.id === data);
-  const meal =[];
-  MEALS.map((meals) => {
-      meals.categoryIds.map((categoryId)=>
-        {if(categoryId===data) meal.push(meals)}
-      )
-  })
-  
+export default function HomeScreen() {
+  const [activeCategory, setActiveCategory] = useState("Beef");
+  const [categories, setCategories] = useState([]);
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    getCategories();
+    getRecipes();
+  }, []);
+
+  const handleChangeCategory = (category) => {
+    getRecipes(category);
+    setActiveCategory(category);
+    setMeals([]);
+  };
+
+  const getCategories = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.themealdb.com/api/json/v1/1/categories.php"
+      );
+      if (response && response.data) {
+        setCategories(response.data.categories);
+        console.log(response.data.categories);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const getRecipes = async (category = "Beef") => {
+    try {
+      const response = await axios.get(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
+      );
+      if (response && response.data) {
+        setMeals(response.data.meals);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <View style={[styles.container, {backgroundColor:category.color}]}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>CATEGORY</Text>
-        <Text style={styles.headerText}>{category.title}</Text>
-      </View>
-      <View style={styles.content}>
-      <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        {meal.map((meals)=>{
-           return (
-            <View key={meals.id} style={styles.mealContainer}>
-              <Button onPress={()=>navigation.navigate('Meal Details',{data:meals})} title={meals.title} imageUrl={meals.imageUrl} style={styles}>
-              </Button>
-            </View>
-           )
-        })}
-      
-      </ScrollView>
-      </View>
-     
-    </View>
+    <View className="flex-1 bg-white">
+      <StatusBar style="dark" />
 
+      <SafeAreaView>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: 50,
+          }}
+          className="space-y-6 pt-14"
+        >
+          {/* Avatar and Bell Icon */}
+          <View className="mx-4 flex-row justify-between items-center">
+            <AdjustmentsHorizontalIcon size={hp(4)} color={"gray"} />
+            <Image
+              source={require("../assets/images/avatar.png")}
+              style={{
+                width: hp(5),
+                height: hp(5),
+                resizeMode: "cover",
+              }}
+              className="rounded-full"
+            />
+          </View>
+
+          {/* Headlines */}
+          <View className="mx-4 space-y-1 mb-2">
+            <View>
+              <Text
+                style={{
+                  fontSize: hp(3.5),
+                }}
+                className="font-bold text-neutral-800"
+              >
+                Fast & Delicious
+              </Text>
+            </View>
+
+            <Text
+              style={{
+                fontSize: hp(3.5),
+              }}
+              className="font-extrabold text-neutral-800"
+            >
+              Food You <Text className="text-[#f64e32]">Love</Text>
+            </Text>
+          </View>
+
+          {/* Search Bar */}
+          <View className="mx-4 flex-row items-center border rounded-xl border-black p-[6px]">
+            <View className="bg-white rounded-full p-2">
+              <MagnifyingGlassIcon
+                size={hp(2.5)}
+                color={"gray"}
+                strokeWidth={3}
+              />
+            </View>
+            <TextInput
+              placeholder="Search Your Favorite Food"
+              placeholderTextColor={"gray"}
+              style={{
+                fontSize: hp(1.7),
+              }}
+              className="flex-1 text-base mb-1 pl-1 tracking-widest"
+            />
+          </View>
+
+          {/* Categories */}
+          <View>
+            {categories.length > 0 && (
+              <Categories
+                categories={categories}
+                activeCategory={activeCategory}
+                handleChangeCategory={handleChangeCategory}
+              />
+            )}
+          </View>
+
+          {/* Recipes Meal */}
+          <View>
+            <Recipes meals={meals} categories={categories} />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-  },
-  header:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  headerText:{
-    fontWeight:'bold',
-    fontSize:30
-  },
-  content:{
-    flex:3,
-  },
-
-  scrollViewContent: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-    padding: 10,
-  },
-  mealContainer: {
-    width: "48%", // Adjust the width to accommodate two items in a row
-    marginBottom: 20,
-    backgroundColor: "#fff",
-    borderRadius: 10,
-    elevation: 3,
-    padding:10
-  },
-  image: {
-    width: "100%",
-    height: 200,
-    borderRadius: 10,
-  },
-});
